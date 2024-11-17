3577bc77a8e31c1ddc6dc5d021b67aa8d2827dc0
HUAN <kiren10122003@gmail.com>
2024-11-17 14:17:22 +0700

    update ver2

--- src/model.py
+++ src/model.py
@@ -1,30 +1,25 @@
-from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from tensorflow.keras.layers import Dense, Dropout, Flatten, BatchNormalization, MaxPooling2D, Conv2D
 
 def build_model(input_shape=(48, 48, 3)):
-    base_model = VGG16(input_shape=input_shape, include_top=False, weights="imagenet")
-
-    for layer in base_model.layers[:-4]:
-        layer.trainable = False
-    
     model = Sequential()
-    model.add(base_model) 
-    
+    model.add(Conv2D(32, (3,3), padding="Same", activation='relu', input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (5,5), padding="Same", activation='relu'))
+    model.add(MaxPooling2D((2,2)))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(64, (3,3), padding="Same", activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (5,5), padding="Same", activation='relu'))
+    model.add(MaxPooling2D((2,2))) 
+    model.add(Dropout(0.5))
+    model.add(Conv2D(128, (3,3), padding="Same", activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (5,5), padding="Same", activation='relu'))
+    model.add(MaxPooling2D((2,2))) 
     model.add(Dropout(0.5))
     model.add(Flatten())
-    model.add(BatchNormalization())
-    model.add(Dense(32, kernel_initializer='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(32, kernel_initializer='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(32, kernel_initializer='he_uniform'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dense(7, activation='softmax'))  
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
     
     return model
