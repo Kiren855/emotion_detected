1c5a29277f841fcd54b28d8193f513e9159c1515
HUAN <kiren10122003@gmail.com>
2024-11-17 14:29:58 +0700

    update ver2

--- src/model.py
+++ src/model.py
@@ -2,24 +2,34 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, BatchNormalization, MaxPooling2D, Conv2D
 
 def build_model(input_shape=(48, 48, 1)):
+    
     model = Sequential()
-    model.add(Conv2D(32, (3,3), padding="Same", activation='relu', input_shape=input_shape))
+    
+    # Block 1
+    model.add(Conv2D(32, (3, 3), padding="Same", activation='relu', input_shape=input_shape))
     model.add(BatchNormalization())
-    model.add(Conv2D(32, (5,5), padding="Same", activation='relu'))
-    model.add(MaxPooling2D((2,2)))
-    model.add(Dropout(0.5))
-    model.add(Conv2D(64, (3,3), padding="Same", activation='relu'))
+    model.add(Conv2D(32, (3, 3), padding="Same", activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.3))
+
+    # Block 2
+    model.add(Conv2D(64, (3, 3), padding="Same", activation='relu'))
     model.add(BatchNormalization())
-    model.add(Conv2D(64, (5,5), padding="Same", activation='relu'))
-    model.add(MaxPooling2D((2,2))) 
-    model.add(Dropout(0.5))
-    model.add(Conv2D(128, (3,3), padding="Same", activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding="Same", activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.3))
+
+    # Block 3
+    model.add(Conv2D(128, (3, 3), padding="Same", activation='relu'))
     model.add(BatchNormalization())
-    model.add(Conv2D(128, (5,5), padding="Same", activation='relu'))
-    model.add(MaxPooling2D((2,2))) 
-    model.add(Dropout(0.5))
+    model.add(Conv2D(128, (3, 3), padding="Same", activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.3))
+
+    # Fully connected layers
     model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(7, activation='softmax'))
     
     return model
