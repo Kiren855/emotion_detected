d2f421a2905bbbc70a76da6f46bc749764debaa7
HUAN <kiren10122003@gmail.com>
2024-11-17 13:10:44 +0700

    update ver2

--- src/model.py
+++ src/model.py
@@ -1,39 +1,39 @@
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from tensorflow.keras.models import Sequential
+import tensorflow as tf
 
 def build_model(input_shape=(48, 48, 1)):
     model = Sequential()
 
     # First convolutional block
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-    model.add(BatchNormalization())
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), strides=1, activation="relu",
+                     padding="same", input_shape=(48, 48, 1)))
+    model.add(Conv2D(64, (3, 3), strides=1, activation="relu",
+                     padding="same"))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     # Second convolutional block
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(128, (3, 3), strides=1, activation="relu",
+                     padding="same", kernel_regularizer=tf.keras.regularizers.l2(0.01)))
+    model.add(Conv2D(256, (3, 3), strides=1, activation="relu",
+                     padding="same", kernel_regularizer=tf.keras.regularizers.l2(0.01)))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     # Third convolutional block
-    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(256, activation="relu"))
     model.add(BatchNormalization())
-    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     # Fully connected layers
-    model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(512, activation="relu"))
     model.add(BatchNormalization())
-    model.add(Dropout(0.5))
-    model.add(Dense(7, activation='softmax'))  
-
+    model.add(Dropout(0.25))
+    model.add(Dense(7, activation="softmax"))
+    
     return model
