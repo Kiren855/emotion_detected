4dceb9e90e4ee0b2cbedccc69acfe72fe81a7e7b
HUAN <kiren10122003@gmail.com>
2024-11-17 16:01:08 +0700

    update ver2

--- src/model.py
+++ src/model.py
@@ -8,53 +8,53 @@ def build_model():
     model.add(Convolution2D(64, (5, 5), padding='same', input_shape=(48, 48, 1)))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  
     model.add(Dropout(0.4))
 
     # Second Conv Block
     model.add(Convolution2D(64, (5, 5), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same')) 
 
     # Third Conv Block
     model.add(Convolution2D(128, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  
     model.add(Dropout(0.3))
 
     # Fourth Conv Block
     model.add(Convolution2D(128, (5, 5), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  
 
     # Fifth Conv Block
     model.add(Convolution2D(256, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  
     model.add(Dropout(0.3))
 
     # Sixth Conv Block
     model.add(Convolution2D(256, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  
 
     # Seventh Conv Block
     model.add(Convolution2D(128, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  
     model.add(Dropout(0.3))
 
     # Eighth Conv Block
     model.add(Convolution2D(64, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('elu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  
     model.add(Dropout(0.3))
 
     # Flatten and Dense layers
