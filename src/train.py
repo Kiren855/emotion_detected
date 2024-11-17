8658ff2b7ac41282948c18cc545d46b6e81e82ae
HUAN <kiren10122003@gmail.com>
2024-11-17 12:28:13 +0700

    update ver2

--- src/train.py
+++ src/train.py
@@ -28,9 +28,9 @@ def train_model(root_dir, batch_size=64, epochs=20, output_model="emotion_model.
 
     history = model.fit(
                 train_gen,
-                steps_per_epoch=len(train_gen),
+                steps_per_epoch=len(train_gen) // batch_size,
                 validation_data=val_gen,
-                validation_steps=len(val_gen), 
+                validation_steps=len(val_gen) // batch_size, 
                 epochs=epochs, 
                 callbacks=[checkpoint],
                 verbose=1
