b94c3402bc98647ce453db51e56d924f18f67d4d
HUAN <kiren10122003@gmail.com>
2024-11-17 11:14:44 +0700

    update model ver 2

--- src/model.py
+++ src/model.py
@@ -1,39 +1,22 @@
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
 from tensorflow.keras.models import Sequential
-import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
 def build_model(input_shape=(48, 48, 1)):
     model = Sequential()
-
-    # First convolutional block
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-    model.add(BatchNormalization())
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    # Second convolutional block
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    # Third convolutional block
-    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    # Fully connected layers
     model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
-    model.add(BatchNormalization())
+    model.add(Dense(1024, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(7, activation='softmax'))  
+    model.add(Dense(7, activation='softmax')) 
 
     return model
